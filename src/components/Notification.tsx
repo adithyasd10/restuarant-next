c73bdebf90e02ab62ef3d4eb561310e1ff404992
Adithya S D <adithyasd@gmail.com>
2025-06-24 14:11:56 +0530

    styling the notification component

---
+++ src/components/Notification.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div>
+      <nav className="flex justify-center items-centerh-5 bg-red-500 font-sans text-white px-1 py-1">
+        Free delivery for all orders over $50.order your food now!
+      </nav>
+    </div>
+  )
+}
+
+export default Notification
