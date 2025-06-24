2efe1a82fd7980cfff9bffafb56df0c248b4f595
Adithya S D <adithyasd@gmail.com>
2025-06-24 18:31:47 +0530

    feat: implement responsive navbar menu with toggle functionality

--- src/components/Carticon.tsx
+++ src/components/Carticon.tsx
@@ -6,10 +6,10 @@ const Carticon = () => {
   return (
     <div>
       <Link href="/carts" className="flex item-center justify-center gap-3">
-        <div className="relative w-8 h-8">
+        <div className="relative w-8 h-8 md:w-[25px] md:h-[25px]">
             <Image src="/cart.png" alt="" fill></Image>
         </div>
-        <span>
+        <span className='flex items-center'>
             Cart (3)
         </span>
       </Link>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,34 @@ import MenuPage from '@/app/menu/page'
 import Link from 'next/link'
 import React from 'react'
 import Menu from './Menu'
+import Carticon from './Carticon'
+import Image from 'next/image'
 
 const Navbar = () => {
+  const user=false
   return (
-    <div className="flex justify-between items-center px-4 py-1 border-b-2 border-b-red-800">
+    <div className="flex justify-between items-center px-4 py-1 border-b-2 border-b-red-800 font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-500 transition-all duration-500 hover:from-orange-600 hover:to-red-600 md:h-24 lg:px-20 xl:40">
+      {/* Left Links*/}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
       {/* LOGO */}
-      <div className="text-2xl font-bold  tracking-widest bg-gradient-to-r from-red-400 via-orange-500 to-yellow-500 text-transparent bg-clip-text transition-transform duration-300 hover:scale-105">
+      <div className="text-2xl font-bold  tracking-widest bg-gradient-to-r from-red-400 via-orange-500 to-yellow-500 text-transparent bg-clip-text transition-transform duration-300 hover:scale-105 flex-1 md:text-center">
         <Link href="/">
         SAVORLY
         </Link>
       </div>
       {/*MENU*/}
-      <div>
+      <div className='md:hidden'>
         <Menu/>
+      </div>
+      {/* Right Links*/}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <Carticon/>
+        {!user ? (<Link href="/login">Login</Link>):
+        (<Link href="/orders">Orders</Link>)}
       </div>
     </div>
   )
