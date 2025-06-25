46c0afa3a0d16c7812b445a88e0af11848595a92
Adithya S D <adithyasd@gmail.com>
2025-06-25 11:02:53 +0530

    footer component implemented

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 border-t-2 border-red-800 flex items-center justify-between font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-500 transition-all duration-500 hover:from-orange-600 hover:to-red-600 ">
+      <Link href="/">SAVORLY</Link>
+      <p> © 2025 Savorly. All rights reserved.</p>
     </div>
   )
 }
