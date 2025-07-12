3e29f4724c3d50a2532d6c76380cf017c920cd84
Adithya S D <adithyasd@gmail.com>
2025-07-12 14:31:47 +0530

    Fix: Removed unused 'id' prop from SingleProductPage

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -21,7 +21,12 @@ const SingleProductPage = () => {
       <div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
         <h1 className="text-3xl font-bold uppercase xl:text-5xl">{singleProduct.title}</h1>
         <p>{singleProduct.desc}</p>
-        <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options}/>
+
+        {/* ✅ FIXED: removed 'id' prop */}
+        <Price 
+          price={singleProduct.price}
+          options={singleProduct.options}
+        />
       </div>
     </div>
   );
